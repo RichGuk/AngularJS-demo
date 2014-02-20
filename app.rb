d3f42bb02c5a4ebecae325eb961d6e88655b022c
Rich Grundy <rich@stinkyink.com>
2014-02-20 11:52:14 +0000

    Moving to single file.

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
 $:.unshift '.'
 require 'sinatra'
 require "sinatra/activerecord"
-require 'models'
 set :database, "sqlite3:///app.db"
+
+class People < ActiveRecord::Base
+end
 
 get '/people' do
   People.all.to_json
